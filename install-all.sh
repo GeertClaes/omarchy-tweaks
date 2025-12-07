e1f081c3154e54258f60cf346a3210dde800f672
Geert Claes <geert.wl.claes@gmail.com>
2025-12-07 11:31:32 +0100

    remove dev tool install

--- install-all.sh
+++ install-all.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
 # Install all packages in order
-./install-asdf.sh
-./install-nodejs.sh
-./install-ruby.sh
 ./install-tmux.sh
 ./install-vscode.sh
 ./install-hyprdynamicmonitors.sh
-./install-stow.sh
 ./install-claude-code.sh
+./install-stow.sh
 ./install-dotfiles.sh
 ./install-t480-fingerprint.sh
