441ef7cb0063e2437774141034049c3e4995b4bd
Geert Claes <geert.wl.claes@gmail.com>
2025-12-07 12:24:03 +0100

    Comment out Claude for now

--- install-all.sh
+++ install-all.sh
@@ -3,7 +3,7 @@
 # Install all packages in order
 ./install-vscode.sh
 ./install-hyprdynamicmonitors.sh
-./install-claude.sh
+#./install-claude.sh
 ./install-stow.sh
 ./install-dotfiles.sh
 ./install-t480-fingerprint.sh
